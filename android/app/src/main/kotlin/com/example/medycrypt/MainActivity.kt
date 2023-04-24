c8dc84dad2fcf7308933cd426d97312db65d5e7f
thecodersuraj <surajthecoder@gmail.com>
2023-04-24 15:21:35 +0545

    initial setup

---
+++ android/app/src/main/kotlin/com/example/medycrypt/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.medycrypt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MedyCryptApp());
+}
+
+class MedyCryptApp extends StatelessWidget {
+  const MedyCryptApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
